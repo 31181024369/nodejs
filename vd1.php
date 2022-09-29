13ad91787a45e942186abe20d844067d7b6ca175
longhoang <long542.nt@gmail.com>
2022-09-29 10:13:12 +0700

    expressjs

---
+++ vd1.php
@@ -0,0 +1,16 @@
+<?php 
+	public function_exists(function_name)
+	{
+
+	}
+	foreach ($variable as $key => $value) {
+		# code...
+	}
+	public function FunctionName($value='')
+	{
+		# code...
+	}
+	while ( <= 10) {
+		# code...
+	}
+ ?>
---
+++ vd2.php
@@ -0,0 +1,11 @@
+<?php
+$a=10;
+$b=10;
+echo $a*$b;
+for( $i=0;$i<$a;$i++)
+{
+    echo "the number is :$i <br>";
+
+}
+
+?>
